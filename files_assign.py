812f7ebc0263ae77dd89617f4c6e81431e4a5b6a
Musthak Ahmad Shaik <mshaikh2@murraystate.edu>
2024-10-20 15:12:36 -0500

    defining funtion for contacts to csv

--- files_assign.py
+++ files_assign.py
@@ -14,3 +14,24 @@ def load_json():
         print("Json file is not found in directory")
     except Exception as e:
         print("error occurred:",e)
+
+json_data = load_json()
+
+import pandas as pd
+
+def contacts_csv(data):
+
+    contacts = []
+    for i in data:
+        for j in i['contacts']:
+            contacts.append({
+                "Adoption ID": i['id'],
+                "Order": j['order'],
+                "Gender": j['gender'],
+                "First Name": j['firstname'],
+                "Last Name": j['lastname']
+            })
+
+    contacts = pd.DataFrame(contacts)
+    contacts.to_csv('contacts.csv')
+    print("csv file created")
